5f22ef206d8bb6571df6db976b3b7f896e94cd96
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-09-04 15:12:21 +0200

    Add relationship test

---
+++ src/test/scala/no/hoopla/serializer/SimpleRelationshipTest.scala
@@ -0,0 +1,40 @@
+package no.hoopla.serializer
+
+import org.json4s.JsonAST.{JArray, JInt, JNull}
+
+object RelationshipObjects {
+  case object PersonSchema extends Schema {
+    override def primaryKey = "id"
+    override def typeName = "persons"
+    override def relationships = List(Relationship(PersonSchema, "boss"))
+  }
+
+  case class Person(id: Int, boss: Option[Person])
+}
+
+import no.hoopla.serializer.RelationshipObjects._
+
+class SimpleRelationshipTest extends UnitSpec {
+  private val boss = Person(1, None)
+  private val personWithBoss = Person(1, Some(boss))
+  
+  "A relationship" should "be null if it is None" in {
+    val serialized = Serializer.serialize(PersonSchema, boss)
+
+    (serialized \ "data" \ "relationships" \ "boss" \ "data") shouldBe JNull
+  }
+
+  it should "be added to 'relationships'" in {
+    val serialized = Serializer.serialize(PersonSchema, personWithBoss)
+
+    assertResult(JInt(boss.id)) {
+      serialized \ "data" \ "relationships" \ "boss" \ "data" \ "id"
+    }
+  }
+
+  it should "not be added to 'included'" in {
+    val serialized = Serializer.serialize(PersonSchema, personWithBoss)
+
+    (serialized \ "included") shouldBe JArray(List())
+  }
+}
