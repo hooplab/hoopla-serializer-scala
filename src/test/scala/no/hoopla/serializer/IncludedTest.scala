9fd9c3f8997ae30789edd05eb7dae72c71a67203
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-09-08 17:10:26 +0200

    Test that included resources does not contain duplicates

--- src/test/scala/no/hoopla/serializer/IncludedTest.scala
+++ src/test/scala/no/hoopla/serializer/IncludedTest.scala
@@ -10,21 +10,55 @@ object IncludedTestData {
     override def relationships = List(Relationship(PersonSchema, "boss", included=true))
   }
   case class Person(id: Long, boss: Option[Person])
+
+  case object UserSchema extends Schema[User] {
+    override def primaryKey: String = "userId"
+    override def typeName: String = "users"
+  }
+  object OrganizationSchema extends Schema[Organization] {
+    override def primaryKey = "organizationId"
+    override def typeName = "organizations"
+    override def relationships = List(
+      Relationship(UserSchema, "admin", included=true),
+      Relationship(UserSchema, "members", included=true)
+    )
+  }
+  case class User(userId: Int)
+  case class Organization(organizationId: Int, admin: User, members: List[User])
 }
 
 class IncludedTest extends UnitSpec {
-
-  private val torstein = Person(1, None)
-  private val torkil = Person(2, Some(torstein))
-  private val magnus = Person(3, Some(torkil))
+  private def identifiesAs(typeName: String, id: Any)(x:JValue): Boolean = {
+    x \ "type" == JString(typeName) && x \ "id" == JString(id.toString)
+  }
 
   "An included resource" should "be added to 'included'" in {
+    val torstein = Person(1, None)
+    val torkil = Person(2, Some(torstein))
+    val magnus = Person(3, Some(torkil))
+
     val serialized = Serializer.serialize(PersonSchema, magnus)
 
     val included = (serialized \ "included").children.filter(_.isInstanceOf[JObject])
 
-    assert(included.exists(x => x \ "type" == JString("persons") && x \ "id" == JString(torkil.id.toString)))
-    assert(included.exists(x => x \ "type" == JString("persons") && x \ "id" == JString(torstein.id.toString)))
-    assert(!included.exists(x => x \ "type" == JString("persons") && x \ "id" == JString(magnus.id.toString)))
+    assertResult(false, "Should not include self")(
+      included.exists(identifiesAs("persons", magnus.id))
+    )
+    assert(included.exists(identifiesAs("persons", torkil.id)))
+    assert(included.exists(identifiesAs("persons", torstein.id)))
+  }
+
+  "Included resources with same type and id" should "only be added once in 'included'" in {
+    val admin = User(1)
+    val users = admin :: User(2) :: Nil
+    val organization = Organization(1, admin, users)
+
+    val serialized = Serializer.serialize(OrganizationSchema, organization)
+
+    val included = (serialized \ "included").children.filter(_.isInstanceOf[JObject])
+
+    assertResult(1, "Admin should only be added once")(
+      included.count(identifiesAs("users", admin.userId))
+    )
   }
 }
