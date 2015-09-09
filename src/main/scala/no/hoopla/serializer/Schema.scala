337842f857c38cf990f6a8f55cff3f960f365eaa
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-09-09 14:56:46 +0200

    Refactor Schema validation

--- src/main/scala/no/hoopla/serializer/Schema.scala
+++ src/main/scala/no/hoopla/serializer/Schema.scala
@@ -2,15 +2,25 @@ package no.hoopla.serializer
 
 import scala.reflect.runtime.universe._
 
-abstract class Schema[T: TypeTag] extends SchemaBase {
-  def fieldIsOfType(field: String, t: Type): Boolean = {
-    typeOf[T].member(TermName(field)).typeSignature.resultType == t
-  }
+abstract class InvalidSchemaException(message: String) extends RuntimeException(message)
 
-  private val missingFields = (primaryKey :: attributes ::: relationships.map(_.attribute)).filter(fieldIsOfType(_, NoType))
-  if (missingFields.nonEmpty) {
-    throw new NoSuchFieldException(s"Class ${typeOf[T]} is missing the following fields: ${missingFields.mkString(", ")}")
+case class MissingFieldsException(t: Type, missingFields: List[String]) extends InvalidSchemaException(
+  s"Class $t is missing the following fields: ${missingFields.mkString(", ")}"
+)
+
+abstract class Schema[T: TypeTag] extends SchemaBase {
+  private def validateSchema(): Unit = {
+    def fieldIsMissingInModel(field: String): Boolean =
+      typeOf[T].member(TermName(field)).typeSignature.resultType == NoType
+
+    val fields = primaryKey :: attributes ::: relationships.map(_.attribute)
+    val missingFields = fields.filter(fieldIsMissingInModel)
+
+    if (missingFields.nonEmpty) {
+      throw MissingFieldsException(typeOf[T], missingFields)
+    }
   }
+  validateSchema()
 }
 
 sealed trait SchemaBase {
