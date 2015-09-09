918ba31d6aae703686221e0f317373c2e39e60a9
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-09-09 16:08:21 +0200

    rename function

--- src/main/scala/no/hoopla/serializer/Schema.scala
+++ src/main/scala/no/hoopla/serializer/Schema.scala
@@ -9,7 +9,7 @@ case class MissingFieldsException(t: Type, missingFields: List[String]) extends 
 )
 
 abstract class Schema[T: TypeTag] extends SchemaBase {
-  private def validateSchema(): Unit = {
+  private def validate(): Unit = {
     def fieldIsMissingInModel(field: String): Boolean =
       typeOf[T].member(TermName(field)).typeSignature.resultType == NoType
 
@@ -20,7 +20,7 @@ abstract class Schema[T: TypeTag] extends SchemaBase {
       throw MissingFieldsException(typeOf[T], missingFields)
     }
   }
-  validateSchema()
+  validate()
 }
 
 sealed trait SchemaBase {
